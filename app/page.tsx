8dc769f4da2f9a0b80a6945e0a1d8ce2708d8eab
William Kamau <william.kamuyu@gapstack.com>
2024-07-04 17:11:55 +0300

    Added top bar filters

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import IconRadioButtonGroup from "@/app/ui/iconRadioButtonGroup";
 import {Input} from "@willikay11/solgates-component-library";
 import SubTopBar from "@/app/ui/subTopBar";
+import SubTopBarFilters from "@/app/ui/subTopBarFilters";
 
 export default function Home() {
   return (
     <main className="h-full grid grid-cols-12 gap-4">
         <div className="col-span-12">
             <SubTopBar />
+            <SubTopBarFilters />
         </div>
       {/*<Image*/}
       {/*    alt="Mountains"*/}
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,2 @@
-import { Inter } from 'next/font/google';
 import localFont from 'next/font/local'
 export const satoshi = localFont({ src: '../Satoshi-Regular.woff' })
-
-export const inter = Inter({ subsets: ['latin'] });
