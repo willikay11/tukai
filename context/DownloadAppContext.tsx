816bb2fa5217eb7f6fe312a8668046470193ec3e
William Kamau <william.kamuyu@gapstack.com>
2025-09-24 18:54:02 +0300

    chore: added links to playstore and app store

--- context/DownloadAppContext.tsx
+++ context/DownloadAppContext.tsx
@@ -1,9 +1,9 @@
 'use client';
 import MobileStore from '@/app/components/mobileStore';
-import { AlertDialog, AlertDialogContent } from '@/components/ui/alert-dialog';
 import { Button } from '@/components/ui/button';
+import { Dialog, DialogContent } from '@/components/ui/dialog';
 import { Separator } from '@/components/ui/separator';
-import { createContext, useContext, useState, ReactNode } from 'react';
+import { createContext, useContext, useState, ReactNode, useEffect } from 'react';
 
 type DownloadAppContextType = {
   onOpen: () => void;
@@ -13,23 +13,53 @@ const DownloadAppContext = createContext<DownloadAppContextType | undefined>(und
 
 export const DownloadAppProvider = ({ children }: { children: ReactNode }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [device, setDevice] = useState<'ios' | 'android' | 'other'>('other');
 
   const onOpen = () => setIsOpen(true);
 
+  const onDownloadApp = () => {
+    let url = 'https://play.google.com/store/apps/details?id=com.tukaitravels.app&hl=en';
+
+    if (device === 'ios') {
+      url = 'https://apps.apple.com/ke/app/tukai/idcom.tukaitravels.app';
+    }
+
+    window.open(url, '_blank');
+  };
+
+  useEffect(() => {
+    const ua = navigator.userAgent || navigator.vendor;
+
+    if (/android/i.test(ua)) {
+      setDevice('android');
+    } else if (/iPad|iPhone|iPod/.test(ua)) {
+      setDevice('ios');
+    } else {
+      setDevice('other');
+    }
+  }, []);
+
   return (
     <DownloadAppContext.Provider value={{ onOpen }}>
-      <AlertDialog open={isOpen}>
-        <AlertDialogContent className="h-fit w-[20rem] max-w-none p-0">
+      <Dialog open={isOpen} onOpenChange={setIsOpen}>
+        <DialogContent className="h-fit w-[20rem] max-w-none p-0">
           <div className="px-4 pt-4">
             <p className="mb-2.5 text-sm text-gray-700">Please download the app to continue</p>
-            <Button className="h-[45px] w-full">Download App</Button>
+            <Button
+              className="h-[45px] w-full"
+              onClick={() => {
+                setIsOpen(false);
+              }}
+            >
+              Download App
+            </Button>
           </div>
           <Separator />
-          <div className="pb-4">
+          <div className="mb-4">
             <MobileStore />
           </div>
-        </AlertDialogContent>
-      </AlertDialog>
+        </DialogContent>
+      </Dialog>
       {children}
     </DownloadAppContext.Provider>
   );
