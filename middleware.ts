471a04fec2dc06fdc6218321dab661184d461dbf
Antony Orenge <antoroko@gmail.com>
2025-12-28 09:03:22 +0200

    feat: add new pages for Help, Privacy, Terms, and Unsubscribe

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
-import { NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 
 export async function middleware(request: NextRequest) {
   const token: any = await getToken({ req: request });
@@ -13,6 +13,10 @@ export async function middleware(request: NextRequest) {
     pathname.startsWith('/experiences') ||
     pathname.startsWith('/communities') ||
     pathname.startsWith('/place') ||
+    pathname.startsWith('/terms') ||
+    pathname.startsWith('/privacy') ||
+    pathname.startsWith('/help') ||
+    pathname.startsWith('/unsubscribe') ||
     (pathname === '/' && !token)
   ) {
     return NextResponse.next();
