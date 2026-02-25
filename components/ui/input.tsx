d2d24458f1128a867647436f2321f3a2811d5636
William Kamau <william.kamuyu@gapstack.com>
2026-02-25 10:50:06 +0300

    feat: update layout and styling for CreateCommunityPage, Input, Textarea, and LocationAutocompleteField components for improved user experience

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -4,12 +4,13 @@ import { cn } from '@/lib/utils';
 
 interface InputProps extends React.ComponentProps<'input'> {
   icon?: React.ReactNode;
+  suffixIcon?: React.ReactNode;
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, icon, ...props }, ref) => {
+  ({ className, type, icon, suffixIcon, ...props }, ref) => {
     return (
-      <div className="flex items-center rounded-[10px] border border-gray-700 border-input px-2 shadow-sm focus-within:border-primary focus-within:outline-none disabled:cursor-not-allowed disabled:opacity-50">
+      <div className="flex items-center rounded-[10px] border border-gray-700 border-input px-3 shadow-sm focus-within:border-primary focus-within:outline-none disabled:cursor-not-allowed disabled:opacity-50">
         {icon && <div className="mr-2">{icon}</div>}
         <input
           type={type}
@@ -20,6 +21,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           ref={ref}
           {...props}
         />
+        {suffixIcon && <div className="ml-2">{suffixIcon}</div>}
       </div>
     );
   },
