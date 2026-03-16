df0361ee86785db04f903f7ad318e976f0f81596
William Kamau <william.kamuyu@gapstack.com>
2026-03-16 07:41:10 +0300

    feat: enhance layout for create and review experience pages with min-height adjustments

--- app/experiences/create/page.tsx
+++ app/experiences/create/page.tsx
@@ -131,7 +131,7 @@ function CreateExperiencePageContent() {
   }
 
   return (
-    <main className="mt-6 grid h-full grid-cols-12 gap-4 px-4 md:px-0">
+    <main className="mt-6 grid min-h-screen grid-cols-12 items-stretch gap-4 px-4 md:px-0">
       <div className="col-span-12 mb-4 md:col-span-10 md:col-start-2 md:mx-0 lg:col-span-4 lg:col-start-3 xl:col-span-4 xl:col-start-3 3xl:col-span-2 3xl:col-start-4 4xl:col-span-2 4xl:col-start-5">
         <CreateExperienceSteps
           currentStep={activeStep}
@@ -153,7 +153,7 @@ function CreateExperiencePageContent() {
           isLoadingExperience={isLoadingExperience}
         />
       </div>
-      <div className="lg:col-span-4 lg:col-start-8">
+      <div className="h-full lg:col-span-4 lg:col-start-8">
         <ExperienceStepSidePanel
           step={activeStep}
           experienceId={experienceId}
